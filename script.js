9f0aeb27a2e14958d2d5e84594f3bb415ffb5ad1
Mikael <dev.kov.mtl@gmail.com>
2025-09-19 11:32:42 -0400

    ace editor config

--- script.js
+++ script.js
@@ -42,3 +42,38 @@ function clearOut() {
 }
 
 $('#clearOut')?.addEventistener('click', clearOut);
+
+// === ACE EDITOR
+function makeEditor(id, more) {
+  const ed = ace.edit(id, {
+    theme: 'ace/theme/dracula',
+    mode,
+    tabSize: 2,
+    useSoftTabs: true,
+    showPrintMargin: false,
+    wrap: true,
+  });
+
+  ed.session.setUseWrapMode(true);
+  ed.commands.addCommand({
+    name: 'run',
+    bindKey: { win: 'Ctrl-Enter', mac: 'Command-Enter' },
+    exec() {
+      runWeb(false);
+    },
+  });
+
+  ed.commands.addCommand({
+    name: 'save',
+    bindKey: { win: 'Ctrl-S', mac: 'Command-S' },
+    exec() {
+      saveProject();
+    },
+  });
+
+  return ed;
+}
+
+const ed_html = makeEditor('ed_html', 'ace/mode/html');
+const ed_css = makeEditor('ed_css', 'ace/mode/css');
+const ed_js = makeEditor('ed_js', 'ace/mode/javascript');
