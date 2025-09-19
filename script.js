77df7418c1f309aae90eafd08e7e584e805af1bf
Mikael <dev.kov.mtl@gmail.com>
2025-09-19 11:01:06 -0400

    utilities func

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log('Hello world');
+// Utilities
+const $ = (s) => document.querySelector(s);
+const $$ = (s) => Array.from(document.querySelectorAll(s));
+const out = $('#output');
+const preview = $('#preview');
+const STORAGE_KEY = 'academy-codelab-web';
+
+const escapeHtml = (s) =>
+  String(s).replace(
+    /[&<>"]/g,
+    (c) =>
+      ({
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt',
+        '"': '&quot',
+      }[c])
+  );
+
+function log(msg, type = 'info') {
+  const color =
+    type === 'error'
+      ? 'var(--err)'
+      : type === 'warn'
+      ? 'var(--warn)'
+      : 'var(--brand)';
+
+  const time = new Date().toLocaleTimeString();
+
+  const line = document.createElement('div');
+
+  line.innerHTML = `<span style="color:${color}">[${time}]</span> ${escapeHtml(
+    msg
+  )}`;
+
+  out.appendChild(line);
+  out.scrollTop = out.scrollHeight;
+}
+
+function clearOut() {
+  out.innerHTML = '';
+}
+
+$('#clearOut')?.addEventistener('click', clearOut);
